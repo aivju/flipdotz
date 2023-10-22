d641bd880b42559997e0c0fa42dea2ccce2c2674
Kai Wolf <kai.wolf@aivju.de>
2023-10-22 10:55:20 +0200

    Add generated files for fonts and widgets

---
+++ lib/widgets/beverage_widget.h
@@ -0,0 +1,18 @@
+// Copyright (c) 2023 aivju GmbH
+// All rights reserved. Redistribution and use in source and binary forms,
+// with or without modification, are not permitted.
+
+#ifndef WIDGETS_BEVERAGE_WIDGET_H_
+#define WIDGETS_BEVERAGE_WIDGET_H_
+
+#include <cstdint>
+
+namespace aivju {
+
+// beer
+const uint16_t kbeerIcon[] = {0x0,    0x380,  0xe80,  0x8f0,  0x3810, 0x2010, 0x3ff0, 0x1020,
+                              0x14b8, 0x14a4, 0x14a4, 0x14a4, 0x14b8, 0x1020, 0x1fe0, 0x0};
+
+}  // namespace aivju
+
+#endif  // WIDGETS_BEVERAGE_WIDGET_H_
