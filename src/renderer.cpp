3ef7236566fed3d095c16aec411e64d181cd0a04
Kai Wolf <kai.wolf@aivju.de>
2023-10-22 19:01:51 +0200

    Add renderer implementation for widgets

---
+++ src/renderer.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2023 aivju GmbH
+// All rights reserved. Redistribution and use in source and binary forms,
+// with or without modification, are not permitted.
+
+#include "renderer.h"
+
+namespace aivju {
+
+Renderer::Renderer(Display* display) : display_(display) {}
+
+void Renderer::drawDisplay() {
+    for (auto& ele : elements_) {
+        ele->render(display_);
+    }
+}
+
+}  // namespace aivju
---
+++ src/renderer.h
@@ -0,0 +1,38 @@
+// Copyright (c) 2023 aivju GmbH
+// All rights reserved. Redistribution and use in source and binary forms,
+// with or without modification, are not permitted.
+
+#ifndef RENDERER_H_
+#define RENDERER_H_
+
+#include <memory>
+#include <vector>
+
+#include "clock_widget.h"
+#include "display.h"
+#include "element.h"
+#include "font.h"
+#include "widget.h"
+#include "macros.h"
+#include "clock_widget.h"
+
+namespace aivju {
+
+class Renderer {
+  public:
+    explicit Renderer(Display* display);
+    void drawDisplay();
+
+    template <typename T>
+    void addWidget(const T& widget) {
+        elements_.push_back(make_unique<T>(widget));
+    }
+
+  private:
+    Display* display_;
+    std::vector<std::unique_ptr<Widget>> elements_;
+};
+
+}  // namespace aivju
+
+#endif  // RENDERER_H_
